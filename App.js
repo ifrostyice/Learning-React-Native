ff902592e3a71aa233cceb245b014231a1f157ec
Tri Aji <ifrosty25@gmail.com>
2018-08-23 14:00:41 +0700

    Pertama Kali

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Haii Haiii! </Text>
+        <Text>Tri Aji Sektiantoro </Text>
+        <Text>XI RPL 4 (32) </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
